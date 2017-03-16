aa8efabab0fab89d2a0111a9663acfef68fde60d
Celal Ceken <celalceken@gmail.com>
2017-03-16 16:57:01 +0300

    Stored XSS v.1.

--- OgrenciEkle.php
+++ OgrenciEkle.php
@@ -126,8 +126,8 @@
 				<td>
 					<label for="soyadi" ><strong>Soyadi</strong></label>
 				</td>
-				<td>
-					<input id="soyadi" name="soyadi" type="text" class="input-mini" placeholder="Öğrenci Soyadını Giriniz" >
+				<td>                                                                                                 <!-- <script>alert(document.cookie);</script>-->
+					<input id="soyadi" name="soyadi" type="text" class="input-mini" placeholder="Öğrenci Soyadını Giriniz" maxlength="15" > //Use zap to by-pass this filter
 				</td>
 			</tr>
 			<tr>
